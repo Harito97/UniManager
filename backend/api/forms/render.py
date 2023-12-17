c754e8795fd0db70a7cbc9ae75bc4b44230949a6
thangmaster37 <nguyenvanthangk66@gmail.com>
2023-12-17 23:35:38 +0700

    update_forgot_password_api

--- backend/api/forms/render.py
+++ backend/api/forms/render.py
@@ -6,6 +6,9 @@ from fastapi.responses import HTMLResponse, RedirectResponse
 import mysql.connector
 
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi_mail import FastMail, MessageSchema, ConnectionConfig, MessageType
+
+from typing import Dict
 
 
 app = FastAPI()
@@ -28,6 +31,10 @@ class User(BaseModel):
     password: str
 
 
+class ForgotPassword(BaseModel):
+    username: str
+
+
 @app.post("/login")
 def login(user: User, request: Request):
     cursor.execute("select ma_sv, pass_word from sinh_vien")
@@ -37,6 +44,50 @@ def login(user: User, request: Request):
         if user.username == account[0] and user.password == account[1]:
             return True
     return False
+
+
+@app.post("/forgot_password")
+async def forgot_password(request: ForgotPassword):
+    # Truy vấn cơ sở dữ liệu để lấy thông tin người dùng dựa trên username (tùy thuộc vào cách bạn cài đặt)
+    # Sau đó, bạn có thể tạo mật khẩu mới và lưu vào cơ sở dữ liệu
+    # Sau khi tạo mật khẩu mới, gửi email chứa mật khẩu mới đến người dùng
+
+    cursor.execute("select ho_ten, email, pass_word from sinh_vien where ma_sv = " + request.username)
+    data = cursor.fetchall()
+
+    if len(data) == 0:
+        return False
+
+    # Cấu hình kết nối cho FastMail
+    conf = ConnectionConfig(
+        MAIL_USERNAME="nguyenvanthang_t66@hus.edu.vn",
+        MAIL_PASSWORD="dpsa liaw qhep rnef",
+        MAIL_FROM="nguyenvanthang_t66@hus.edu.vn",
+        MAIL_PORT=587,
+        MAIL_SERVER="smtp.gmail.com",
+        MAIL_STARTTLS = True,
+        MAIL_SSL_TLS = False,
+        USE_CREDENTIALS=True,
+        VALIDATE_CERTS=True,
+        MAIL_FROM_NAME="Phong Dao Tao DHKHTN",
+    )
+
+    # Khởi tạo FastMail
+    fastMail = FastMail(conf)
+
+    # Tạo nội dung email
+    email_content = f"Dear {data[0][0]}. Your password is: {data[0][2]} <br> Please do not disclose login information to others!"
+
+    # Gửi email
+    message = MessageSchema(
+        subject="Retrieve Your Higher Education Portal Login Password",
+        recipients=[data[0][1]],
+        body=email_content,
+        subtype=MessageType.html,
+    )
+    await fastMail.send_message(message)
+
+    return True
 
 
     
--- frontend/src/components/LandingPage/LoginPopup/Forgot.jsx
+++ frontend/src/components/LandingPage/LoginPopup/Forgot.jsx
@@ -1,15 +1,38 @@
 import React from "react";
 import { Form, Input, Button } from "antd";
 import { UserOutlined } from "@ant-design/icons";
+import axios from "axios";
 
 const Forgot = ({ handleForgot }) => {
+
+  const handleSubmit = async (values) => {
+
+    try {
+      const response = await axios.post('http://127.0.0.1:8000/forgot_password', {
+        username: values.username,
+      });
+      
+      if (response.data) {
+        alert("Mật khẩu đã được gửi qua email của bạn!")
+      } 
+      
+      else {
+        alert("Người dùng không tồn tại!");
+      }; 
+    } 
+    
+    catch (error) {
+      console.error(error);
+    } 
+  };
+
   return (
     <>
       <div className="p-5">
         <h1 className="mb-4 text-center text-2xl font-semibold text-gray-600">
           QUÊN MẬT KHẨU
         </h1>
-        <Form layout="vertical">
+        <Form layout="vertical" onFinish={handleSubmit}>
           <Form.Item
             label="Tên đăng nhập:"
             name="username"
