87e5c160174394d79938cdb5c5acf297b4932a0f
Hideyoshi <96051070+DucAnh1053@users.noreply.github.com>
2023-12-27 22:19:20 +0700

    Update Teacher Dashboard

--- frontend/src/components/Teacher/Dashboard.jsx
+++ frontend/src/components/Teacher/Dashboard.jsx
@@ -1,9 +1,100 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Button, Table, Modal } from "antd";
+import Manager from "./Manager";
 
-const Dashboard = ({user}) => {
+const Dashboard = ({ user }) => {
+  const columns = [
+    {
+      title: "STT",
+      dataIndex: "index",
+      render: (text, record, index) => index + 1,
+      width: 50,
+    },
+    { title: "Môn học", dataIndex: "ten_hp", width: 300 },
+    {
+      title: "Lớp môn học",
+      dataIndex: "ma_hp_lop",
+      width: 100,
+      render: (_, record) => (
+        <p>
+          {record.ma_hp} {record.ma_lop}
+        </p>
+      ),
+    },
+    { title: "Số SV", dataIndex: "da_dk", width: 60 },
+    {
+      title: "Lịch học",
+      dataIndex: "lich_hoc",
+      width: 200,
+      render: (_, record) => {
+        const time_data = record.lich_hoc;
+        return time_data.map((t) => (
+          <p>
+            {t.thu}-({t.bd}-{t.kt})-{t.phong}
+          </p>
+        ));
+      },
+    },
+    {
+      title: "Xem lớp",
+      dataIndex: "view",
+      width: 100,
+      render: (_, record) => (
+        <Button onClick={() => showInfo(record)}>Chi tiết</Button>
+      ),
+    },
+  ];
+
+  const [selectedRow, setSelectedRow] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const [classData, setClassData] = useState([]);
+
+  const showInfo = (record) => {
+    setSelectedRow(record);
+    setShowModal(true);
+  };
+
+  const handleModalCancel = () => {
+    setShowModal(false);
+  };
+
+  // Data mẫu
+  const data = [
+    {
+      ma_lh: 1,
+      ten_hp: "Cơ sở dữ liệu Web và hệ thống thông tin",
+      ma_hp: "MAT3385",
+      ma_lop: 1,
+      da_dk: 30,
+      lich_hoc: [
+        { thu: "T2", bd: 1, kt: 2, phong: "103T4" },
+        { thu: "T5", bd: 6, kt: 10, phong: "PM" },
+      ],
+    },
+  ];
+
   return (
-    <div>Dashboard</div>
-  )
-}
+    <>
+      <h1 className="pb-5 text-xl font-bold">Các lớp đang giảng dạy</h1>
+      <Table columns={columns} dataSource={data} pagination={false}></Table>
+      <Modal
+        open={showModal}
+        onCancel={handleModalCancel}
+        footer={null}
+        width="auto" // Đặt độ rộng là "auto"
+        className="overflow-auto"
+      >
+        {selectedRow && (
+          <>
+            <h1 className="text-xl font-bold pb-5">
+              Danh sách sinh viên lớp {selectedRow.ma_hp} {selectedRow.ma_lop}
+            </h1>
+            <Manager ma_lh={selectedRow.ma_lh} />
+          </>
+        )}
+      </Modal>
+    </>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
--- frontend/src/components/Teacher/Manager.jsx
+++ frontend/src/components/Teacher/Manager.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { Table } from "antd";
 
-const Manager = ({user}) => {
+const Manager = ({ ma_lh }) => {
+  const columns = [
+    {
+      title: "STT",
+      dataIndex: "index",
+      render: (text, record, index) => index + 1,
+      width: 50,
+    },
+    { title: "Họ và tên", dataIndex: "ho_ten", width: 200 },
+    { title: "Điểm TX", dataIndex: "diem_tx", width: 60 },
+    { title: "Hệ số TX", dataIndex: "he_so_tx", width: 60 },
+    { title: "Điểm GK", dataIndex: "diem_gk", width: 60 },
+    { title: "Hệ số GK", dataIndex: "he_so_gk", width: 60 },
+    { title: "Điểm CK", dataIndex: "diem_ck", width: 60 },
+    { title: "Hệ số CK", dataIndex: "he_so_ck", width: 60 },
+  ];
   return (
-    <div>Manager</div>
-  )
-}
+    <>
+     <Table columns={columns} scroll={{ x: 610 }} size="small"></Table>
+    </>
+  );
+};
 
-export default Manager
+export default Manager;
