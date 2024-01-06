e8933b09adf16522c5575e9ec1bbc2945309dca3
Hideyoshi <96051070+DucAnh1053@users.noreply.github.com>
2024-01-06 11:25:00 +0700

    Update animation

--- frontend/src/components/Home/LoginForm.jsx
+++ frontend/src/components/Home/LoginForm.jsx
@@ -15,37 +15,38 @@ const LoginPopup = () => {
 
   return (
     <>
-      <div className="flex min-h-screen items-center justify-center backdrop-blur-sm">
-        <div className="relative m-6 flex flex-col space-y-8 rounded-2xl bg-white shadow-2xl md:flex-row md:space-y-0">
-          <div className="flex flex-col justify-center p-8 md:p-14">
-            <a
-              href="#"
-              class="mb-6 flex items-center text-2xl font-semibold text-gray-900 dark:text-white"
-            >
+      <AnimatePresence>
+        <div className="flex min-h-screen items-center justify-center backdrop-blur-sm">
+          <motion.div
+            layout
+            className="relative m-6 flex flex-col space-y-8 rounded-2xl bg-white shadow-2xl md:flex-row md:space-y-0"
+          >
+            <div className="flex flex-col justify-center p-8 md:p-14">
+              <a
+                href="#"
+                class="mb-6 flex items-center text-2xl font-semibold text-gray-900 dark:text-white"
+              >
+                <img class="mr-2 h-8 w-8" src={HusLogo} alt="logo" />
+                HUS
+              </a>
+              {/* Login Form section */}
+              {showForgot ? (
+                <Forgot handleForgot={handleForgot} />
+              ) : (
+                <Login handleForgot={handleForgot} />
+              )}
+              {/* Image section section */}
+            </div>
+            <div class="relative">
               <img
-                class="mr-2 h-8 w-8"
-                src={HusLogo}
-                alt="logo"
+                src={showForgot ? ForgotImg : LoginImg}
+                alt="img"
+                class="hidden h-full w-[400px] rounded-r-2xl object-cover md:block"
               />
-              HUS
-            </a>
-            {/* Login Form section */}
-            {showForgot ? (
-              <Forgot handleForgot={handleForgot} />
-            ) : (
-              <Login handleForgot={handleForgot} />
-            )}
-            {/* Image section section */}
-          </div>
-          <div class="relative">
-            <img
-              src={showForgot ? ForgotImg : LoginImg}
-              alt="img"
-              class="hidden h-full w-[400px] rounded-r-2xl object-cover md:block"
-            />
-          </div>
+            </div>
+          </motion.div>
         </div>
-      </div>
+      </AnimatePresence>
     </>
   );
 };
