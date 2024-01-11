829c7b24ae30b3a6194326a9ca0196d29efd887f
Hideyoshi <96051070+DucAnh1053@users.noreply.github.com>
2024-01-11 09:14:16 +0700

    Admin Page Layout

---
+++ frontend/src/components/Admin/Sidebar.jsx
@@ -0,0 +1,62 @@
+import React, { useEffect } from "react";
+import { Menu } from "antd";
+import { Link } from "react-router-dom";
+import {
+  AppstoreOutlined,
+  ReconciliationOutlined,
+  CalendarOutlined,
+  AppstoreAddOutlined,
+  FileTextOutlined,
+  QuestionCircleOutlined,
+} from "@ant-design/icons";
+import Logo from "../../assets/hus-logo.svg";
+
+const Sidebar = () => {
+  let path = window.location.pathname;
+  console.log(path);
+
+  useEffect(() => {}, []);
+
+  const selectedKey = () => {
+    if (path === "/admin/dashboard" || path === "/admin") {
+      return "1";
+    } else if (path === "/admin/student_manager") {
+      return "2";
+    } else if (path === "/admin/teacher_manager") {
+      return "3";
+    }
+  };
+
+  return (
+    <>
+      <div className="flex w-full flex-col items-center justify-center bg-[#EBEBEB] py-4">
+        <img src={Logo} alt="logo" className="w-10" />
+      </div>
+      <Menu
+        theme="light"
+        mode="inline"
+        defaultSelectedKeys={[selectedKey()]}
+        className="bg-[#EBEBEB] text-base font-normal text-black"
+        items={[
+          {
+            key: "1",
+            icon: <AppstoreOutlined />,
+            label: <Link to="/admin/dashboard">Tổng quan</Link>,
+          },
+          {
+            key: "2",
+            icon: <CalendarOutlined />,
+            label: <Link to="/admin/student_manager">Quản lý sinh viên</Link>,
+          },
+          {
+            key: "3",
+            icon: <CalendarOutlined />,
+            label: <Link to="/admin/teacher_manager">Quản lý giáo viên</Link>,
+          },
+        ]}
+      />
+    </>
+  );
+};
+
+export default Sidebar;
