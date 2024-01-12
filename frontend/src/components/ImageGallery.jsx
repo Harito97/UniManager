0e8cb639c521160670e096e3e95fa0d48bf69a40
Hideyoshi <96051070+DucAnh1053@users.noreply.github.com>
2024-01-12 16:53:15 +0700

    Update

--- frontend/src/components/ImageGallery.jsx
+++ frontend/src/components/ImageGallery.jsx
@@ -1,22 +1,23 @@
 import React from "react";
-import bg from "../assets/background.jpg"
+import bg from "../assets/background.jpg";
 
 const ImageGallery = () => {
   return (
     <>
       <div class="relative">
         <img
-          src="https://source.unsplash.com/1600x900/?nature"
+          src="https://source.unsplash.com/1000x1000/?nature"
           alt="img"
           class="h-full rounded-xl object-cover"
         />
 
         <div class="absolute bottom-10 right-6 rounded bg-white bg-opacity-30 p-6 drop-shadow-lg backdrop-blur-sm">
-          <span class="text-xl text-white">
-            We've been uesing Untitle to kick"
+          <span class="xl:text-xl text-white">
+            It does not matter
             <br />
-            start every new project and can't <br />
-            imagine working without it."
+            how slowly you go
+            <br />
+            as long as you do not stop.
           </span>
         </div>
       </div>
