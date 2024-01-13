6cbcf31c88ffc4e1346b22518889da48e8ca5946
Hideyoshi <96051070+DucAnh1053@users.noreply.github.com>
2024-01-13 10:45:11 +0700

    Update

--- frontend/src/components/ImageGallery.jsx
+++ frontend/src/components/ImageGallery.jsx
@@ -1,25 +1,37 @@
-import React from "react";
-import bg from "../assets/background.jpg";
+import React, { useEffect, useState } from "react";
+import PacmanLoader from "react-spinners/PacmanLoader";
 
 const ImageGallery = () => {
+  const [shouldRender, setShouldRender] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShouldRender(true);
+    }, 1000);
+  }, []);
+
   return (
     <>
       <div className="relative">
         <img
-          src="https://source.unsplash.com/1000x1000/?nature"
+          src="https://source.unsplash.com/900x900/?nature"
           alt="img"
-          className="h-full rounded-xl object-cover"
+          className="rounded-xl object-cover"
         />
 
-        <div className="absolute bottom-10 right-6 rounded bg-white bg-opacity-30 p-6 drop-shadow-lg backdrop-blur-sm">
-          <span className="xl:text-xl text-white">
-            It does not matter
-            <br />
-            how slowly you go
-            <br />
-            as long as you do not stop.
-          </span>
-        </div>
+        {shouldRender && (
+          <>
+            <div className="absolute bottom-10 right-6 rounded bg-white bg-opacity-30 p-6 drop-shadow-lg backdrop-blur-sm">
+              <span className="text-white xl:text-xl">
+                It does not matter
+                <br />
+                how slowly you go
+                <br />
+                as long as you do not stop.
+              </span>
+            </div>
+          </>
+        )}
       </div>
     </>
   );
